8dc34b2466f5ee923054b4a57c46355d31e1e10a
Dan Cutting <dan@cutting.io>
2020-02-07 14:13:17 +0000

    Try to extract some common Metal code.

---
+++ Metal1/MetalContext.swift
@@ -0,0 +1,56 @@
+import Metal
+import MetalKit
+
+final class MetalContext: NSObject {
+    
+    let device: MTLDevice
+    let view: MTKView
+    let pipelineState: MTLRenderPipelineState
+    let commandQueue: MTLCommandQueue
+    let onRender: () -> Void
+
+    init(onRender: @escaping () -> Void) {
+        device = MetalContext.makeDevice()!
+        view = MetalContext.makeView(device: device)
+        pipelineState = MetalContext.makePipelineState(device: device, metalView: view)
+        commandQueue = device.makeCommandQueue()!
+        self.onRender = onRender
+        super.init()
+        view.delegate = self
+    }
+
+    private static func makeDevice() -> MTLDevice? {
+        MTLCreateSystemDefaultDevice()
+    }
+    
+    private static func makeView(device: MTLDevice) -> MTKView {
+        let metalView = MTKView(frame: NSRect(x: 0.0, y: 0.0, width: 800.0, height: 600.0))
+        metalView.device = device
+        metalView.preferredFramesPerSecond = 60
+        metalView.colorPixelFormat = .bgra8Unorm
+        metalView.framebufferOnly = true
+        return metalView
+    }
+    
+    private static func makePipelineState(device: MTLDevice, metalView: MTKView) -> MTLRenderPipelineState {
+        let defaultLibrary = device.makeDefaultLibrary()!
+        let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
+        
+        return try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+    }
+}
+
+extension MetalContext: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    }
+    
+    func draw(in view: MTKView) {
+        onRender()
+    }
+}
--- Metal1/MetalViewController.swift
+++ Metal1/MetalViewController.swift
@@ -4,12 +4,7 @@ import MetalKit
 
 class MetalViewController: NSViewController {
     
-    var device: MTLDevice!
-    var metalView: MTKView!
-    var vertexBuffer: MTLBuffer!
-    var pipelineState: MTLRenderPipelineState!
-    var commandQueue: MTLCommandQueue!
-    
+    var metalContext: MetalContext!
     var frameCounter = 0
     
     var vertexData: [Float] = [
@@ -17,7 +12,8 @@ class MetalViewController: NSViewController {
         -1.0, -1.0, 0.0,
         1.0, -1.0, 0.0
     ]
-    
+    var vertexBuffer: MTLBuffer!
+
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -25,63 +21,36 @@ class MetalViewController: NSViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+        
     override func loadView() {
-        device = MTLCreateSystemDefaultDevice()
-
-        metalView = MTKView(frame: NSRect(x: 0.0, y: 0.0, width: 800.0, height: 600.0))
-        metalView.device = device
-        metalView.preferredFramesPerSecond = 60
-        metalView.colorPixelFormat = .bgra8Unorm
-        metalView.framebufferOnly = true
-        metalView.delegate = self
-        
-        view = metalView
-        
+        metalContext = MetalContext { [weak self] in self?.render() }
+        view = metalContext.view
+        vertexBuffer = makeVertexBuffer(device: metalContext.device)
+    }
+    
+    private func makeVertexBuffer(device: MTLDevice) -> MTLBuffer {
         let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
-        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [.storageModeShared])
-        
-        let defaultLibrary = device.makeDefaultLibrary()!
-        let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
-        let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
-        
-        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
-        pipelineStateDescriptor.vertexFunction = vertexProgram
-        pipelineStateDescriptor.fragmentFunction = fragmentProgram
-        pipelineStateDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
-        
-        pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
-        
-        commandQueue = device.makeCommandQueue()
+        return device.makeBuffer(bytes: vertexData, length: dataSize, options: [.storageModeShared])!
     }
     
     private func render() {
         guard
-            let renderPassDescriptor = metalView.currentRenderPassDescriptor,
-            let drawable = metalView.currentDrawable
+            let renderPassDescriptor = metalContext.view.currentRenderPassDescriptor,
+            let drawable = metalContext.view.currentDrawable
         else { return }
 
         print(frameCounter)
         frameCounter += 1
 
-        let commandBuffer = commandQueue.makeCommandBuffer()!
+        let commandBuffer = metalContext.commandQueue.makeCommandBuffer()!
         
         let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
-        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.setRenderPipelineState(metalContext.pipelineState)
         renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
         renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
         renderEncoder.endEncoding()
         
         commandBuffer.present(drawable)
         commandBuffer.commit()
-    }
-}
-
-extension MetalViewController: MTKViewDelegate {
-    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-    }
-    
-    func draw(in view: MTKView) {
-        render()
     }
 }
