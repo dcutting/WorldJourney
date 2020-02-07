250c8a7523373d82216641f9030e779cc5cee036
Dan Cutting <dan@cutting.io>
2020-02-07 21:09:49 +0000

    Larger mesh.

--- Metal1/MetalViewController.swift
+++ Metal1/MetalViewController.swift
@@ -13,6 +13,7 @@ class MetalViewController: NSViewController {
         1.0, -1.0, 0.0
     ]
     var vertexBuffer: MTLBuffer!
+    var vertexCount = 0
 
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -25,12 +26,37 @@ class MetalViewController: NSViewController {
     override func loadView() {
         metalContext = MetalContext { [weak self] in self?.render() }
         view = metalContext.view
-        vertexBuffer = makeVertexBuffer(device: metalContext.device)
+        (vertexBuffer, vertexCount) = makeVertexBuffer(device: metalContext.device)
     }
     
-    private func makeVertexBuffer(device: MTLDevice) -> MTLBuffer {
-        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
-        return device.makeBuffer(bytes: vertexData, length: dataSize, options: [.storageModeShared])!
+    private func makeMesh() -> ([Float], Int) {
+        var data = [Float]()
+        let size: Float = 1.0
+        let x = 10
+        let y = 10
+        for j in (0..<y) {
+            for i in (0..<x) {
+                let quad = makeQuad(atX: Float(i) * size, y: Float(j) * size, size: size)
+                data.append(contentsOf: quad)
+            }
+        }
+        return (data, x*y*2*3)
+    }
+    
+    private func makeQuad(atX x: Float, y: Float, size: Float) -> [Float] {
+        let inset = size * 0.9
+        let a = [ x, y, 0 ]
+        let b = [ x + inset, y, 0 ]
+        let c = [ x, y + inset, 0 ]
+        let d = [ x + inset, y + inset, 0]
+        return [ a, b, d, d, c, a ].flatMap { $0 }
+    }
+    
+    private func makeVertexBuffer(device: MTLDevice) -> (MTLBuffer, Int) {
+        let (data, count) = makeMesh()
+        let dataSize = data.count * MemoryLayout.size(ofValue: data[0])
+        let buffer = device.makeBuffer(bytes: data, length: dataSize, options: [.storageModeShared])!
+        return (buffer, count)
     }
     
     struct Uniforms {
@@ -53,9 +79,9 @@ class MetalViewController: NSViewController {
         renderEncoder.setRenderPipelineState(metalContext.pipelineState)
         renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
         
-        let angle = Float(frameCounter) / Float(metalContext.view.preferredFramesPerSecond) * 4
+        let angle = Float(frameCounter) / Float(metalContext.view.preferredFramesPerSecond) / 2
         print(angle)
-        let translation = float4x4(translationBy: SIMD3<Float>(0.0, 0.0, -5.0))
+        let translation = float4x4(translationBy: SIMD3<Float>(0.0, 0.0, -20.0))
         let rotation = float4x4(rotationAbout: SIMD3<Float>(0.0, 1.0, 0.0), by: angle)
         
         var uniforms = Uniforms(
@@ -65,7 +91,7 @@ class MetalViewController: NSViewController {
         let dataSize = MemoryLayout<Uniforms>.size
         
         renderEncoder.setVertexBytes(&uniforms, length: dataSize, index: 1)
-        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertexCount, instanceCount: 1)
         renderEncoder.endEncoding()
         
         commandBuffer.present(drawable)
--- Metal1/Shaders.metal
+++ Metal1/Shaders.metal
@@ -11,18 +11,31 @@ struct Uniforms {
     float4x4 projectionMatrix;
 };
 
+// Generate a random float in the range [0.0f, 1.0f] using x, y, and z (based on the xor128 algorithm)
+float rand(int x, int y, int z)
+{
+    int seed = x + y * 57 + z * 241;
+    seed= (seed<< 13) ^ seed;
+    return (( 1.0 - ( (seed * (seed * seed * 15731 + 789221) + 1376312589) & 2147483647) / 1073741824.0f) + 1.0f) / 2.0f;
+}
+
 vertex RasteriserData basic_vertex(const device packed_float3* vertex_array [[buffer(0)]],
                                    constant Uniforms &uniforms [[buffer(1)]],
                                    unsigned int vid [[vertex_id]]) {
     float4 v = float4(vertex_array[vid], 1.0);
     
+    v += rand(v.x, v.y, v.y);
+    
     // Color corners.
     float4 c = float4(1.0, 0.0, 0.0, 1.0);
-    if (v.y > 0.0) {
+    if (vid % 2 == 0) {
         c = float4(0.0, 0.0, 1.0, 1.0);
-    } else if (v.x < 0.0) {
-        c = float4(0.0, 1.0, 0.0, 1.0);
     }
+//    if (v.y > 0.0) {
+//        c = float4(0.0, 0.0, 1.0, 1.0);
+//    } else if (v.x < 0.0) {
+//        c = float4(0.0, 1.0, 0.0, 1.0);
+//    }
     
     float4 projected = uniforms.projectionMatrix * uniforms.modelMatrix * v;
     
