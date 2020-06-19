1eb911dbdd2304e155805c96d4a87aa5fe291c52
Dan Cutting <dan@cutting.io>
2020-06-19 01:23:01 +0100

    Refactor.

--- WorldJourney/Renderer.swift
+++ WorldJourney/Renderer.swift
@@ -1,26 +1,30 @@
 import Metal
 import MetalKit
 
-final class Renderer: NSObject {
+class Renderer: NSObject {
     
     let device: MTLDevice
     let view: MTKView
     let pipelineState: MTLRenderPipelineState
     let depthStencilState: MTLDepthStencilState
     let commandQueue: MTLCommandQueue
-    let onRender: () -> Void
+    var frameCounter = 0
+    var vertexBuffer: MTLBuffer!
+    var vertexCount = 0
+    let halfGridWidth = 75
+    var surfaceDistance: Float = 50.0
 
-    init(onRender: @escaping () -> Void) {
+    override init() {
         device = Renderer.makeDevice()!
         view = Renderer.makeView(device: device)
         pipelineState = Renderer.makePipelineState(device: device, metalView: view)
         depthStencilState = Renderer.makeDepthStencilState(device: device)!
         commandQueue = device.makeCommandQueue()!
-        self.onRender = onRender
         super.init()
         view.delegate = self
+        (vertexBuffer, vertexCount) = makeVertexBuffer(device: device)
     }
-
+    
     private static func makeDevice() -> MTLDevice? {
         MTLCreateSystemDefaultDevice()
     }
@@ -48,7 +52,7 @@ final class Renderer: NSObject {
         
         return try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
     }
-
+    
     private static func makeDepthStencilState(device: MTLDevice) -> MTLDepthStencilState? {
         let depthStencilDescriptor = MTLDepthStencilDescriptor()
         depthStencilDescriptor.depthCompareFunction = .less
@@ -62,6 +66,73 @@ extension Renderer: MTKViewDelegate {
     }
     
     func draw(in view: MTKView) {
-        onRender()
+        guard
+            let renderPassDescriptor = view.currentRenderPassDescriptor,
+            let drawable = view.currentDrawable
+            else { return }
+        
+        frameCounter += 1
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()!
+        
+        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+        //        renderEncoder.setTriangleFillMode(.lines)
+        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.setDepthStencilState(depthStencilState)
+        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        
+        let worldRadius: Float = 1.0
+        let frequency: Float = 3.0/worldRadius
+        let mountainHeight: Float = worldRadius * 0.02
+        let surface: Float = (worldRadius + mountainHeight) * 1.05
+        surfaceDistance *= 0.99
+        let distance: Float = surface + surfaceDistance
+        
+        let eye = SIMD3<Float>(0, 0, distance)
+        
+        let gridWidth = Int32(halfGridWidth * 2)
+        
+        var uniforms = Uniforms(
+            worldRadius: worldRadius,
+            frequency: frequency,
+            amplitude: mountainHeight,
+            gridWidth: gridWidth,
+            cameraPosition: eye,
+            viewMatrix: makeViewMatrix(eye: eye),
+            modelMatrix: makeModelMatrix(),
+            projectionMatrix: makeProjectionMatrix()
+        )
+        
+        let dataSize = MemoryLayout<Uniforms>.size
+        renderEncoder.setVertexBytes(&uniforms, length: dataSize, index: 1)
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertexCount, instanceCount: 1)
+        renderEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+
+    private func makeVertexBuffer(device: MTLDevice) -> (MTLBuffer, Int) {
+        let (data, count) = makeGridMesh(n: halfGridWidth)
+        let dataSize = data.count * MemoryLayout.size(ofValue: data[0])
+        let buffer = device.makeBuffer(bytes: data, length: dataSize, options: [.storageModeShared])!
+        return (buffer, count)
+    }
+    
+    private func makeViewMatrix(eye: SIMD3<Float>) -> float4x4 {
+        let at = SIMD3<Float>(0.0, 1.0, 0.0)
+        let up = SIMD3<Float>(0.0, 1.0, 0.0)
+        return look(at: at, eye: eye, up: up)
+    }
+    
+    private func makeModelMatrix() -> float4x4 {
+        let angle: Float = Float(frameCounter) / Float(view.preferredFramesPerSecond) / 10
+        let spin = float4x4(rotationAbout: SIMD3<Float>(0.0, 1.0, 0.6), by: -angle)
+        return spin
+    }
+    
+    private func makeProjectionMatrix() -> float4x4 {
+        let aspectRatio: Float = Float(view.bounds.width) / Float(view.bounds.height)
+        return float4x4(perspectiveProjectionFov: Float.pi / 3, aspectRatio: aspectRatio, nearZ: 0.001, farZ: 1000.0)
     }
 }
--- WorldJourney/ViewController.swift
+++ WorldJourney/ViewController.swift
@@ -1,19 +1,9 @@
 import AppKit
-import Metal
-import MetalKit
 
 class ViewController: NSViewController {
     
-    var metalContext: Renderer!
-    var frameCounter = 0
+    var renderer: Renderer!
     
-    var vertexBuffer: MTLBuffer!
-    var vertexCount = 0
-    
-    let halfGridWidth = 75
-
-    var surfaceDistance: Float = 50.0
-
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -21,81 +11,9 @@ class ViewController: NSViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-        
+    
     override func loadView() {
-        metalContext = Renderer { [weak self] in self?.render() }
-        view = metalContext.view
-        (vertexBuffer, vertexCount) = makeVertexBuffer(device: metalContext.device)
-    }
-        
-    private func makeVertexBuffer(device: MTLDevice) -> (MTLBuffer, Int) {
-        let (data, count) = makeGridMesh(n: halfGridWidth)
-        let dataSize = data.count * MemoryLayout.size(ofValue: data[0])
-        let buffer = device.makeBuffer(bytes: data, length: dataSize, options: [.storageModeShared])!
-        return (buffer, count)
-    }
-    
-    private func render() {
-        guard
-            let renderPassDescriptor = metalContext.view.currentRenderPassDescriptor,
-            let drawable = metalContext.view.currentDrawable
-        else { return }
-
-        frameCounter += 1
-
-        let commandBuffer = metalContext.commandQueue.makeCommandBuffer()!
-        
-        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
-//        renderEncoder.setTriangleFillMode(.lines)
-        renderEncoder.setRenderPipelineState(metalContext.pipelineState)
-        renderEncoder.setDepthStencilState(metalContext.depthStencilState)
-        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-                
-        let worldRadius: Float = 1.0
-        let frequency: Float = 3.0/worldRadius
-        let mountainHeight: Float = worldRadius * 0.02
-        let surface: Float = (worldRadius + mountainHeight) * 1.05
-        surfaceDistance *= 0.99
-        let distance: Float = surface + surfaceDistance
-        
-        let eye = SIMD3<Float>(0, 0, distance)
-
-        let gridWidth = Int32(halfGridWidth * 2)
-        
-        var uniforms = Uniforms(
-            worldRadius: worldRadius,
-            frequency: frequency,
-            amplitude: mountainHeight,
-            gridWidth: gridWidth,
-            cameraPosition: eye,
-            viewMatrix: makeViewMatrix(eye: eye),
-            modelMatrix: makeModelMatrix(),
-            projectionMatrix: makeProjectionMatrix()
-        )
-        
-        let dataSize = MemoryLayout<Uniforms>.size
-        renderEncoder.setVertexBytes(&uniforms, length: dataSize, index: 1)
-        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertexCount, instanceCount: 1)
-        renderEncoder.endEncoding()
-        
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
-    }
-    
-    private func makeViewMatrix(eye: SIMD3<Float>) -> float4x4 {
-        let at = SIMD3<Float>(0.0, 1.0, 0.0)
-        let up = SIMD3<Float>(0.0, 1.0, 0.0)
-        return look(at: at, eye: eye, up: up)
-    }
-    
-    private func makeModelMatrix() -> float4x4 {
-        let angle: Float = Float(frameCounter) / Float(metalContext.view.preferredFramesPerSecond) / 10
-        let spin = float4x4(rotationAbout: SIMD3<Float>(0.0, 1.0, 0.6), by: -angle)
-        return spin
-    }
-    
-    private func makeProjectionMatrix() -> float4x4 {
-        let aspectRatio: Float = Float(metalContext.view.bounds.width) / Float(metalContext.view.bounds.height)
-        return float4x4(perspectiveProjectionFov: Float.pi / 3, aspectRatio: aspectRatio, nearZ: 0.001, farZ: 1000.0)
+        renderer = Renderer()
+        view = renderer.view
     }
 }
