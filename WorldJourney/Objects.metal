ec26bf1f4fefc599b6991f1ce8b64b85fa70f897
Dan Cutting <dan@cutting.io>
2021-08-02 15:17:16 +1000

    Basic USDZ model support.

--- WorldJourney/Objects.metal
+++ WorldJourney/Objects.metal
@@ -3,16 +3,29 @@
 
 using namespace metal;
 
+enum {
+    textureIndexBaseColor,
+    textureIndexMetallic,
+    textureIndexRoughness,
+    textureIndexNormal,
+    textureIndexEmissive,
+    textureIndexIrradiance = 9
+};
+
 struct ObjectIn {
-    float3 position  [[attribute(0)]];
-    float3 normal    [[attribute(1)]];
-    float2 texCoords [[attribute(2)]];
+  float3 position [[attribute(0)]];
+  float3 normal [[attribute(1)]];
+  float3 tangent [[attribute(2)]];
+  float2 texCoords [[attribute(3)]];
 };
 
 struct ObjectOut {
-    float4 position [[position]];
-    float3 normal;
-    float2 texCoords;
+  float4 clipPosition [[position]];
+  float4 worldPosition;
+  float3 normal;
+  float3 tangent;
+  float3 bitangent;
+  float2 texCoords;
 };
 
 struct GbufferOut {
@@ -24,21 +37,160 @@ struct GbufferOut {
 vertex ObjectOut object_vertex(ObjectIn vertexIn [[stage_in]],
                                constant Uniforms &uniforms [[buffer(1)]],
                                constant InstanceUniforms *instanceUniforms [[buffer(2)]],
+                               texture2d<float> baseColorMap    [[texture(0)]],
+                               texture2d<float> metallicMap     [[texture(1)]],
+                               texture2d<float> roughnessMap    [[texture(2)]],
+                               texture2d<float> normalMap       [[texture(3)]],
+                               texture2d<float> emissiveMap     [[texture(4)]],
+                               texturecube<float> irradianceMap [[texture(5)]],
                                ushort iid [[instance_id]]) {
-  ObjectOut vertexOut;
   matrix_float4x4 modelMatrix = instanceUniforms[iid].modelMatrix;
-  vertexOut.position = uniforms.projectionMatrix * uniforms.viewMatrix * modelMatrix * float4(vertexIn.position, 1);
+  float4 worldPosition = modelMatrix * float4(vertexIn.position, 1);
   matrix_float4x4 modelMatrixIT = instanceUniforms[iid].modelNormalMatrix;
-  vertexOut.normal = normalize((modelMatrixIT * float4(vertexIn.normal, 1))).xyz;
+
+  ObjectOut vertexOut;
+  vertexOut.clipPosition = uniforms.projectionMatrix * uniforms.viewMatrix * worldPosition;
+  vertexOut.worldPosition = worldPosition;
+  vertexOut.normal = (modelMatrixIT * float4(normalize(vertexIn.normal), 1)).xyz;
+  vertexOut.tangent = (modelMatrixIT * float4(normalize(vertexIn.tangent), 1)).xyz;
+  vertexOut.bitangent = (modelMatrixIT * float4(cross(normalize(vertexIn.normal), normalize(vertexIn.tangent)), 1)).xyz;
   vertexOut.texCoords = vertexIn.texCoords;
   return vertexOut;
 }
 
-fragment GbufferOut object_fragment(ObjectOut in [[stage_in]],
-                                constant Uniforms &uniforms [[buffer(0)]]) {
-  return {
-    .albedo = float4(0, 1, 0, 1),
-    .normal = float4(in.normal, 1),
-    .position = in.position
-  };
+//constexpr sampler s(coord::normalized, address::repeat, filter::linear, mip_filter::linear);
+
+//fragment GbufferOut object_fragment(ObjectOut in [[stage_in]],
+//                                    constant Uniforms &uniforms [[buffer(0)]],
+//                                    texture2d<float> texture [[texture(0)]]) {
+//  float4 albedo = texture.sample(s, in.texCoords);
+//  return {
+//    .albedo = albedo,
+//    .normal = float4(in.normal, 1),
+//    .position = in.worldPosition
+//  };
+//}
+
+struct LightingParameters {
+  float3 lightDir;
+  float3 viewDir;
+  float3 halfVector;
+  float3 reflectedVector;
+  float3 normal;
+  float3 reflectedColor;
+  float3 irradiatedColor;
+  float3 baseColor;
+  float3 diffuseLightColor;
+  float  NdotH;
+  float  NdotV;
+  float  NdotL;
+  float  HdotL;
+  float  metalness;
+  float  roughness;
+};
+
+#define SRGB_ALPHA 0.055
+
+float linear_from_srgb(float x) {
+    if (x <= 0.04045)
+        return x / 12.92;
+    else
+        return powr((x + SRGB_ALPHA) / (1.0 + SRGB_ALPHA), 2.4);
+}
+
+float3 linear_from_srgb(float3 rgb) {
+    return float3(linear_from_srgb(rgb.r), linear_from_srgb(rgb.g), linear_from_srgb(rgb.b));
+}
+
+static float3 diffuseTerm(LightingParameters parameters) {
+    float3 diffuseColor = (parameters.baseColor.rgb / M_PI_F) * (1.0 - parameters.metalness);
+    return diffuseColor * parameters.NdotL * parameters.diffuseLightColor;
+}
+
+static float SchlickFresnel(float dotProduct) {
+    return pow(clamp(1.0 - dotProduct, 0.0, 1.0), 5.0);
+}
+
+static float Geometry(float NdotV, float alphaG) {
+    float a = alphaG * alphaG;
+    float b = NdotV * NdotV;
+    return 1.0 / (NdotV + sqrt(a + b - a * b));
+}
+
+static float TrowbridgeReitzNDF(float NdotH, float roughness) {
+    if (roughness >= 1.0)
+        return 1.0 / M_PI_F;
+    
+    float roughnessSqr = roughness * roughness;
+    
+    float d = (NdotH * roughnessSqr - NdotH) * NdotH + 1;
+    return roughnessSqr / (M_PI_F * d * d);
+}
+
+static float3 specularTerm(LightingParameters parameters) {
+    float specularRoughness = parameters.roughness * (1.0 - parameters.metalness) + parameters.metalness;
+    
+    float D = TrowbridgeReitzNDF(parameters.NdotH, specularRoughness);
+    
+    float Cspec0 = 0.04;
+    float3 F = mix(Cspec0, 1, SchlickFresnel(parameters.HdotL));
+    float alphaG = powr(specularRoughness * 0.5 + 0.5, 2);
+    float G = Geometry(parameters.NdotL, alphaG) * Geometry(parameters.NdotV, alphaG);
+    
+    float3 specularOutput = (D * G * F * parameters.irradiatedColor) * (1.0 + parameters.metalness * parameters.baseColor) +
+                                                 parameters.irradiatedColor * parameters.metalness * parameters.baseColor;
+    
+    return specularOutput;
+}
+
+fragment GbufferOut object_fragment(ObjectOut in                     [[stage_in]],
+                             constant Uniforms &uniforms      [[buffer(0)]],
+                             texture2d<float> baseColorMap    [[texture(textureIndexBaseColor)]],
+                             texture2d<float> metallicMap     [[texture(textureIndexMetallic)]],
+                             texture2d<float> roughnessMap    [[texture(textureIndexRoughness)]],
+                             texture2d<float> normalMap       [[texture(textureIndexNormal)]],
+                             texture2d<float> emissiveMap     [[texture(textureIndexEmissive)]],
+                             texturecube<float> irradianceMap [[texture(textureIndexIrradiance)]])
+{
+    constexpr sampler linearSampler (mip_filter::linear, mag_filter::linear, min_filter::linear);
+    constexpr sampler mipSampler(min_filter::linear, mag_filter::linear, mip_filter::linear);
+    constexpr sampler normalSampler(filter::nearest);
+    
+    const float3 diffuseLightColor(4);
+
+    LightingParameters parameters;
+
+    float4 baseColor = baseColorMap.sample(linearSampler, in.texCoords);
+    parameters.baseColor = linear_from_srgb(baseColor.rgb);
+    parameters.roughness = roughnessMap.sample(linearSampler, in.texCoords).g;
+    parameters.metalness = metallicMap.sample(linearSampler, in.texCoords).b;
+    float3 mapNormal = normalMap.sample(normalSampler, in.texCoords).rgb * 2.0 - 1.0;
+    //mapNormal.y = -mapNormal.y; // Flip normal map Y-axis if necessary
+    float3x3 TBN(normalize(in.tangent), normalize(in.bitangent), normalize(in.normal));
+    parameters.normal = normalize(TBN * mapNormal);
+
+    parameters.diffuseLightColor = diffuseLightColor;
+  parameters.lightDir = normalize(uniforms.sunPosition - in.worldPosition.xyz);// uniforms.directionalLightInvDirection;
+    parameters.viewDir = normalize(uniforms.cameraPosition - in.worldPosition.xyz);
+    parameters.halfVector = normalize(parameters.lightDir + parameters.viewDir);
+    parameters.reflectedVector = reflect(-parameters.viewDir, parameters.normal);
+
+    parameters.NdotL = saturate(dot(parameters.normal, parameters.lightDir));
+    parameters.NdotH = saturate(dot(parameters.normal, parameters.halfVector));
+    parameters.NdotV = saturate(dot(parameters.normal, parameters.viewDir));
+    parameters.HdotL = saturate(dot(parameters.lightDir, parameters.halfVector));
+
+    float mipLevel = parameters.roughness * irradianceMap.get_num_mip_levels();
+    parameters.irradiatedColor = irradianceMap.sample(mipSampler, parameters.reflectedVector, level(mipLevel)).rgb;
+    
+    float3 emissiveColor = emissiveMap.sample(linearSampler, in.texCoords).rgb;
+
+  float3 color = diffuseTerm(parameters) + specularTerm(parameters) + emissiveColor;
+
+  float4 albedo = float4(float3(color), baseColor.a);
+    return {
+      .albedo = albedo,
+      .normal = float4(normalize(in.normal), 1),
+      .position = in.worldPosition
+    };
 }
--- WorldJourney/Objects.swift
+++ WorldJourney/Objects.swift
@@ -5,6 +5,7 @@ class Objects {
   var objectPipelineState: MTLRenderPipelineState!
   var vertexDescriptor: MDLVertexDescriptor!
   var objectRenderPassDescriptor: MTLRenderPassDescriptor!
+  var sourceMeshes: [MDLMesh] = []
   var objectMeshes: [MTKMesh] = []
   var albedoTexture: MTLTexture!
   var normalTexture: MTLTexture!
@@ -12,11 +13,32 @@ class Objects {
   var depthTexture: MTLTexture!
   var instanceUniformsBuffer: MTLBuffer!
   var numInstances = 10
+  var nodes = [Node]()
+  let defaultTexture: MTLTexture
+  let defaultNormalMap: MTLTexture
 
   init(device: MTLDevice, library: MTLLibrary) {
+    (defaultTexture, defaultNormalMap) = Self.buildDefaultTextures(device: device)
+
     (vertexDescriptor, objectPipelineState) = makeObjectDescriptors(device: device, library: library)
     loadObjects(device: device, library: library)
     makeInstanceUniforms(device: device)
+  }
+
+  static func buildDefaultTextures(device: MTLDevice) -> (MTLTexture, MTLTexture) {
+      let bounds = MTLRegionMake2D(0, 0, 1, 1)
+      let descriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .rgba8Unorm,
+                                                                width: bounds.size.width,
+                                                                height: bounds.size.height,
+                                                                mipmapped: false)
+      descriptor.usage = .shaderRead
+      let defaultTexture = device.makeTexture(descriptor: descriptor)!
+      let defaultColor: [UInt8] = [ 0, 0, 0, 255 ]
+      defaultTexture.replace(region: bounds, mipmapLevel: 0, withBytes: defaultColor, bytesPerRow: 4)
+      let defaultNormalMap = device.makeTexture(descriptor: descriptor)!
+      let defaultNormal: [UInt8] = [ 127, 127, 255, 255 ]
+      defaultNormalMap.replace(region: bounds, mipmapLevel: 0, withBytes: defaultNormal, bytesPerRow: 4)
+      return (defaultTexture, defaultNormalMap)
   }
 
   private func makeObjectDescriptors(device: MTLDevice, library: MTLLibrary) -> (MDLVertexDescriptor, MTLRenderPipelineState) {
@@ -30,12 +52,26 @@ class Objects {
     descriptor.vertexFunction = library.makeFunction(name: "object_vertex")
     descriptor.fragmentFunction = library.makeFunction(name: "object_fragment")
     
+    
     let vertexDescriptor = MDLVertexDescriptor()
-    vertexDescriptor.attributes[0] = MDLVertexAttribute(name: MDLVertexAttributePosition, format: .float3, offset: 0, bufferIndex: 0)
-    vertexDescriptor.attributes[1] = MDLVertexAttribute(name: MDLVertexAttributeNormal, format: .float3, offset: MemoryLayout<Float>.size * 3, bufferIndex: 0)
-    vertexDescriptor.attributes[2] = MDLVertexAttribute(name: MDLVertexAttributeTextureCoordinate, format: .float2, offset: MemoryLayout<Float>.size * 6, bufferIndex: 0)
-    vertexDescriptor.layouts[0] = MDLVertexBufferLayout(stride: MemoryLayout<simd_float3>.size + MemoryLayout<simd_float3>.size + MemoryLayout<simd_float2>.size)
-
+    vertexDescriptor.attributes[0] = MDLVertexAttribute(name: MDLVertexAttributePosition,
+                                                        format: .float3,
+                                                        offset: 0,
+                                                        bufferIndex: 0)
+    vertexDescriptor.attributes[1] = MDLVertexAttribute(name: MDLVertexAttributeNormal,
+                                                        format: .float3,
+                                                        offset: MemoryLayout<Float>.size * 3,
+                                                        bufferIndex: 0)
+    vertexDescriptor.attributes[2] = MDLVertexAttribute(name: MDLVertexAttributeTangent,
+                                                        format: .float3,
+                                                        offset: MemoryLayout<Float>.size * 6,
+                                                        bufferIndex: 0)
+    vertexDescriptor.attributes[3] = MDLVertexAttribute(name: MDLVertexAttributeTextureCoordinate,
+                                                        format: .float2,
+                                                        offset: MemoryLayout<Float>.size * 9,
+                                                        bufferIndex: 0)
+    vertexDescriptor.layouts[0] = MDLVertexBufferLayout(stride: MemoryLayout<Float>.size * 11)
+    
     descriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(vertexDescriptor)
 
     let state = try! device.makeRenderPipelineState(descriptor: descriptor)
@@ -45,19 +81,38 @@ class Objects {
   
   private func loadObjects(device: MTLDevice, library: MTLLibrary) {
     let bufferAllocator = MTKMeshBufferAllocator(device: device)
-    let modelURL = Bundle.main.url(forResource: "toy_biplane", withExtension: "usdz")!
+    let modelURL = Bundle.main.url(forResource: "Moss_Rock_14_Free_Rock_Pack_Vol", withExtension: "usdz")!
     let asset = MDLAsset(url: modelURL, vertexDescriptor: vertexDescriptor, bufferAllocator: bufferAllocator)
+    asset.loadTextures()
+
+    for sourceMesh in asset.childObjects(of: MDLMesh.self) as! [MDLMesh] {
+        sourceMesh.addOrthTanBasis(forTextureCoordinateAttributeNamed: MDLVertexAttributeTextureCoordinate,
+                                   normalAttributeNamed: MDLVertexAttributeNormal,
+                                   tangentAttributeNamed: MDLVertexAttributeTangent)
+        sourceMesh.vertexDescriptor = vertexDescriptor
+    }
     
     do {
-        (_, objectMeshes) = try MTKMesh.newMeshes(asset: asset, device: device)
+        (sourceMeshes, objectMeshes) = try MTKMesh.newMeshes(asset: asset, device: device)
     } catch {
         fatalError("Could not extract meshes from Model I/O asset")
+    }
+    
+    let textureLoader = MTKTextureLoader(device: device)
+    for (sourceMesh, mesh) in zip(sourceMeshes, objectMeshes) {
+        var materials = [Material]()
+        for sourceSubmesh in sourceMesh.submeshes as! [MDLSubmesh] {
+            let material = Material(material: sourceSubmesh.material, textureLoader: textureLoader)
+            materials.append(material)
+        }
+        let node = Node(mesh: mesh, materials: materials)
+        nodes.append(node)
     }
   }
   
   func makeInstanceUniforms(device: MTLDevice) {
     let instanceUniforms = (0..<numInstances).map { i -> InstanceUniforms in
-      let modelMatrix = matrix_float4x4(translationBy: SIMD3<Float>(x: Float(i-(numInstances/2)) * 500, y: 5500, z: 0)) * matrix_float4x4(scaleBy: 10)
+      let modelMatrix = matrix_float4x4(translationBy: SIMD3<Float>(x: Float(i-(numInstances/2)) * 500, y: 5150, z: 0)) * matrix_float4x4(scaleBy: 10)
       let modelNormalMatrix = modelMatrix.inverse.transpose
       return InstanceUniforms(modelMatrix: modelMatrix, modelNormalMatrix: modelNormalMatrix)
     }
@@ -91,19 +146,41 @@ class Objects {
     self.objectRenderPassDescriptor = objectRenderPassDescriptor
   }
   
+  enum TextureIndex: Int {
+      case baseColor
+      case metallic
+      case roughness
+      case normal
+      case emissive
+      case irradiance = 9
+  }
+
   func renderObjects(renderEncoder: MTLRenderCommandEncoder, uniforms: Uniforms, compositor: Compositor) {
     renderEncoder.setRenderPipelineState(objectPipelineState)
     renderEncoder.setDepthStencilState(compositor.depthStencilState)
     var uniforms = uniforms
-    for mesh in objectMeshes {
-
-      let vertexBuffer = mesh.vertexBuffers.first!
-      renderEncoder.setVertexBuffer(vertexBuffer.buffer, offset: vertexBuffer.offset, index: 0)
+    for node in nodes {
+      
+      let mesh = node.mesh
+      
       renderEncoder.setVertexBytes(&uniforms, length: MemoryLayout<Uniforms>.stride, index: 1)
       renderEncoder.setVertexBuffer(instanceUniformsBuffer, offset: 0, index: 2)
       renderEncoder.setFragmentBytes(&uniforms, length: MemoryLayout<Uniforms>.stride, index: 0)
-      
-      for submesh in mesh.submeshes {
+
+
+      for (bufferIndex, vertexBuffer) in mesh.vertexBuffers.enumerated() {
+        renderEncoder.setVertexBuffer(vertexBuffer.buffer, offset: vertexBuffer.offset, index: bufferIndex)
+      }
+
+      for (submeshIndex, submesh) in mesh.submeshes.enumerated() {
+        let material = node.materials[submeshIndex]
+
+      renderEncoder.setFragmentTexture(material.baseColor ?? defaultTexture, index: TextureIndex.baseColor.rawValue)
+      renderEncoder.setFragmentTexture(material.metallic ?? defaultTexture, index: TextureIndex.metallic.rawValue)
+      renderEncoder.setFragmentTexture(material.roughness ?? defaultTexture, index: TextureIndex.roughness.rawValue)
+      renderEncoder.setFragmentTexture(material.normal ?? defaultNormalMap, index: TextureIndex.normal.rawValue)
+      renderEncoder.setFragmentTexture(material.emissive ?? defaultTexture, index: TextureIndex.emissive.rawValue)
+
         let indexBuffer = submesh.indexBuffer
         renderEncoder.drawIndexedPrimitives(type: submesh.primitiveType,
                                             indexCount: submesh.indexCount,
@@ -111,7 +188,43 @@ class Objects {
                                             indexBuffer: indexBuffer.buffer,
                                             indexBufferOffset: indexBuffer.offset,
                                             instanceCount: numInstances)
-      }
     }
+    }
+  }
+}
+
+class Node {
+    var modelMatrix: float4x4
+    let mesh: MTKMesh
+    let materials: [Material]
+ 
+    init(mesh: MTKMesh, materials: [Material]) {
+        modelMatrix = matrix_identity_float4x4
+        self.mesh = mesh
+        self.materials = materials
+    }
+}
+
+class Material {
+  var baseColor: MTLTexture?
+  var metallic: MTLTexture?
+  var roughness: MTLTexture?
+  var normal: MTLTexture?
+  var emissive: MTLTexture?
+  
+  init(material sourceMaterial: MDLMaterial?, textureLoader: MTKTextureLoader) {
+    baseColor = texture(for: .baseColor, in: sourceMaterial, textureLoader: textureLoader)
+    metallic = texture(for: .metallic, in: sourceMaterial, textureLoader: textureLoader)
+    roughness = texture(for: .roughness, in: sourceMaterial, textureLoader: textureLoader)
+    normal = texture(for: .tangentSpaceNormal, in: sourceMaterial, textureLoader: textureLoader)
+    emissive = texture(for: .emission, in: sourceMaterial, textureLoader: textureLoader)
+  }
+
+  func texture(for semantic: MDLMaterialSemantic, in material: MDLMaterial?, textureLoader: MTKTextureLoader) -> MTLTexture? {
+    guard let materialProperty = material?.property(with: semantic) else { return nil }
+    guard let sourceTexture = materialProperty.textureSamplerValue?.texture else { return nil }
+    let wantMips = false//materialProperty.semantic != .tangentSpaceNormal
+    let options: [MTKTextureLoader.Option : Any] = [ .generateMipmaps : wantMips ]
+    return try? textureLoader.newTexture(texture: sourceTexture, options: options)
   }
 }
