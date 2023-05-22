6d1c2536d9aa7d54b52228cadf91eec61b3f628b
Dan Cutting <dan@cutting.io>
2023-05-22 10:23:55 +1000

    Tidy up.

--- Terrainium/GameViewController.swift
+++ Terrainium/GameViewController.swift
@@ -1,45 +1,27 @@
-//
-//  GameViewController.swift
-//  Terrainium
-//
-//  Created by Dan Cutting on 21/12/2022.
-//  Copyright © 2022 cutting.io. All rights reserved.
-//
-
 import Cocoa
 import MetalKit
 
-// Our macOS specific view controller
 class GameViewController: NSViewController {
-
-    var renderer: Renderer!
-    var mtkView: MTKView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        guard let mtkView = self.view as? MTKView else {
-            print("View attached to GameViewController is not an MTKView")
-            return
-        }
-
-        // Select the device to render with.  We choose the default device
-        guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
-            print("Metal is not supported on this device")
-            return
-        }
-
-        mtkView.device = defaultDevice
-
-        guard let newRenderer = Renderer(metalKitView: mtkView) else {
-            print("Renderer cannot be initialized")
-            return
-        }
-
-        renderer = newRenderer
-
-        renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
-
-        mtkView.delegate = renderer
+  var renderer: Renderer!
+  var mtkView: MTKView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    guard let mtkView = self.view as? MTKView else {
+      print("View attached to GameViewController is not an MTKView")
+      return
     }
+    guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
+      print("Metal is not supported on this device")
+      return
+    }
+    mtkView.device = defaultDevice
+    guard let newRenderer = Renderer(metalKitView: mtkView) else {
+      print("Renderer cannot be initialized")
+      return
+    }
+    renderer = newRenderer
+    renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
+    mtkView.delegate = renderer
+  }
 }
