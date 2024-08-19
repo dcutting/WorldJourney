f3cc3a2c2af5a5dfef78360b44054f6c443f6964
Dan Cutting <dan@cutting.io>
2024-08-19 16:59:42 +1000

    Add SmallWorldRacing target.

---
+++ SmallWorldRacing macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SmallWorldRacing macOS
+//
+//  Created by Dan Cutting on 19/8/2024.
+//  Copyright © 2024 cutting.io. All rights reserved.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
