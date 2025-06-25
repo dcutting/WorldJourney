4bba2949dbafd491c036a4592dc0ebd536ce5244
Dan Cutting <dan@cutting.io>
2025-06-25 18:36:35 +1000

    Use physics for cube sphere.

--- CubeSphere Shared/Renderer.swift
+++ CubeSphere Shared/Renderer.swift
@@ -1,4 +1,5 @@
 import MetalKit
+import PhyKit
 
 final class Renderer: NSObject, MTKViewDelegate {
   var diagnosticMode: Int32 = 0
@@ -13,10 +14,19 @@ final class Renderer: NSObject, MTKViewDelegate {
   private var farZ: Double { dAltitude + 3 * dRadius }
   private var baseRingLevel: Int32 = 1
   private let maximumRingLevel: Int32 = 19
-  
+  private var lastGPUEndTime: CFTimeInterval = 0
+
   private var dStartTime: Double = 0
   private var dTime: Double = 0
-  private var dEye: simd_double3 = .zero
+  private var dEye: simd_double3 {
+    get {
+      simd_double3(physics.avatar.position.simd)
+    }
+    set {
+      physics.halt()
+      physics.avatar.position = simd_float3(newValue).phyVector3
+    }
+  }
   private let dSun = simd_double3(repeating: 105_781_668_823)
 
   private var dRadius: Double { Double(iRadius) }
@@ -30,24 +40,25 @@ final class Renderer: NSObject, MTKViewDelegate {
   private var fEye: simd_float3 { simd_float3(dEye) }
   private var fSun: simd_float3 { simd_float3(dSun) }
 
+  private let physics = Physics(planetMass: 6e16, moveAmount: 200000)
+
   func adjust(heightM: Double) {
     dEye.y = heightM
   }
   
   private func reset() {
     dStartTime = CACurrentMediaTime()
-    dEye = simd_double3(1000, 150000, 1000)
+    dEye = simd_double3(1000, 20000, 1000)
   }
   
   private func gameLoop(screenWidth: Double, screenHeight: Double) -> Uniforms {
     readInput()
     updateClock()
-//    updateWorld()
     updateLod()
     printStats()
 
     let uniforms = Uniforms(
-      mvp: simd_float4x4(makeMVP(width: screenWidth, height: screenHeight)),
+      mvp: makeMVP(width: screenWidth, height: screenHeight),
       eye: fEye,
       sun: fSun,
       ringCenterEyeOffset: ringCenterEyeOffset,
@@ -63,62 +74,83 @@ final class Renderer: NSObject, MTKViewDelegate {
   }
 
   private func readInput() {
-    var speed = 10.0
-    if Keyboard.IsKeyPressed(.shift) {
-      speed *= 5000.0
-    }
-    if Keyboard.IsKeyPressed(.space) {
-      speed *= 100.0
-    }
+    // Craft control.
 
+    // Translation.
     if Keyboard.IsKeyPressed(KeyCodes.w) {
-      dEye.z -= speed
+      physics.forward()
     }
     if Keyboard.IsKeyPressed(KeyCodes.s) {
-      dEye.z += speed
+      physics.back()
     }
     if Keyboard.IsKeyPressed(KeyCodes.a) {
-      dEye.x -= speed
+      physics.strafeLeft()
     }
     if Keyboard.IsKeyPressed(KeyCodes.d) {
-      dEye.x += speed
-    }
-    if Keyboard.IsKeyPressed(KeyCodes.q) {
-      dEye.y -= speed
+      physics.strafeRight()
     }
     if Keyboard.IsKeyPressed(KeyCodes.e) {
-      dEye.y += speed
+      physics.strafeUp()
+    }
+    if Keyboard.IsKeyPressed(KeyCodes.q) {
+      physics.strafeDown()
+    }
+    // Attitude.
+    if Keyboard.IsKeyPressed(KeyCodes.j) {
+      physics.turnLeft()
+    }
+    if Keyboard.IsKeyPressed(KeyCodes.l) {
+      physics.turnRight()
+    }
+    if Keyboard.IsKeyPressed(KeyCodes.i) {
+      physics.turnDown()
+    }
+    if Keyboard.IsKeyPressed(KeyCodes.k) {
+      physics.turnUp()
+    }
+    if Keyboard.IsKeyPressed(KeyCodes.u) {
+      physics.rollLeft()
+    }
+    if Keyboard.IsKeyPressed(KeyCodes.o) {
+      physics.rollRight()
+    }
+    // Boost.
+    if Keyboard.IsKeyPressed(KeyCodes.space) {
+      physics.strafeUp()
+    }
+    if Keyboard.IsKeyPressed(KeyCodes.returnKey) {
+      physics.halt()
     }
 
     // Locations.
     if Keyboard.IsKeyPressed(KeyCodes.z) {
-      dEye = simd_double3(1000, 12, 1000)
+      dEye = simd_double3(1000, 100, 1000)
     }
     if Keyboard.IsKeyPressed(KeyCodes.x) {
-      dEye = simd_double3(1000, 120, 1000)
+      dEye = simd_double3(1000, 1000, 1000)
     }
     if Keyboard.IsKeyPressed(KeyCodes.c) {
-      dEye = simd_double3(1000, 1200, 1000)
+      dEye = simd_double3(1000, 3000, 1000)
     }
-    if Keyboard.IsKeyPressed(KeyCodes.f) {
+    if Keyboard.IsKeyPressed(KeyCodes.c) {
       dEye = simd_double3(1000, 6000, 1000)
     }
-    if Keyboard.IsKeyPressed(KeyCodes.v) {
+    if Keyboard.IsKeyPressed(KeyCodes.f) {
       dEye = simd_double3(1000, 12000, 1000)
     }
-    if Keyboard.IsKeyPressed(KeyCodes.g) {
+    if Keyboard.IsKeyPressed(KeyCodes.v) {
       dEye = simd_double3(1000, 60000, 1000)
     }
-    if Keyboard.IsKeyPressed(KeyCodes.b) {
+    if Keyboard.IsKeyPressed(KeyCodes.g) {
       dEye = simd_double3(1000, 120000, 1000)
     }
-    if Keyboard.IsKeyPressed(KeyCodes.h) {
+    if Keyboard.IsKeyPressed(KeyCodes.b) {
       dEye = simd_double3(1000, 60000, 1000)
     }
-    if Keyboard.IsKeyPressed(KeyCodes.n) {
+    if Keyboard.IsKeyPressed(KeyCodes.h) {
       dEye = simd_double3(1000, 1200000, 1000)
     }
-    if Keyboard.IsKeyPressed(KeyCodes.j) {
+    if Keyboard.IsKeyPressed(KeyCodes.n) {
       dEye = simd_double3(1000, 6000000, 1000)
     }
     if Keyboard.IsKeyPressed(KeyCodes.m) {
@@ -150,17 +182,6 @@ final class Renderer: NSObject, MTKViewDelegate {
     dTime = CACurrentMediaTime() - dStartTime
   }
   
-  private func updateWorld() {
-    let distance =  dTime * mps
-    let base: Double = dAmplitude * 4.9
-    let top = dRadius * 1//0.5
-    let altitude = base// + ((top - base) * max(0, sin(-dTime * 0.05) * 0.5 + 0.2))
-    let x: Double = dRadius - distance// + 2000.31397 * cos(distance * 0.0005)
-    let y: Double = altitude
-    let z: Double = dRadius + 20000//-dRadius + 5.4314 * distance
-    dEye = simd_double3(x, y, z)
-  }
-
   private func updateLod() {
     let msl = max(1, dAltitude)
     let ring = Int32(floor(log2(msl / 1000))) + 4 // TODO: how to find base ring level? This is based on sea level, but should be based on calculated terrain height.
@@ -171,32 +192,31 @@ final class Renderer: NSObject, MTKViewDelegate {
     maximumRingLevel - baseRingLevel + 1
   }
 
-  private func makeMVP(width: Double, height: Double) -> double4x4 {
-    let at = simd_double3(dEye.x, 0, 0)
-    let up = simd_double3(0, 1, 0)
-
-    let relativeAt = at - dEye
-    let viewMatrix = look(at: relativeAt, eye: .zero, up: up)
+  private func makeMVP(width: Double, height: Double) -> float4x4 {
+    let viewMatrix = physics.avatar.transform.simd.inverse
     let perspectiveMatrix = double4x4(perspectiveProjectionFov: fov, aspectRatio: width / height, nearZ: nearZ, farZ: farZ)
-    let mvp = perspectiveMatrix * viewMatrix;
+    let mvp = float4x4(perspectiveMatrix) * viewMatrix;
     return mvp
   }
   
   private func printStats() {
-    let ringCenterCellString = String(format: "(%d, %d)", iRingCenterCell.x, iRingCenterCell.y)
+//    let ringCenterCellString = String(format: "(%d, %d)", iRingCenterCell.x, iRingCenterCell.y)
     let eyeString = String(format: "(%.2f, %.2f, %.2f)", dEye.x, dEye.y, dEye.z)
-    let coreString = String(format: "%.2fkm", dEye.y / 1000.0)
+//    let coreString = String(format: "%.2fkm", dEye.y / 1000.0)
     let altitudeString = abs(dAltitude) < 1000.0 ? String(format: "%.1fm", dAltitude) : String(format: "%.2fkm", dAltitude / 1000.0)
+    let velocity = length(physics.avatar.linearVelocity.simd)
+    let speedString = velocity.isFinite ? String(format: "%.1fkm/h", velocity * 3.6) : "N/A"
     let timeString = String(format: "%.2fs", dTime)
     print(
       timeString,
       " Ring:", baseRingLevel,
-      " Cell:", ringCenterCellString,
+//      " Cell:", ringCenterCellString,
       " Eye:", eyeString,
-      " Core:", coreString,
+//      " Core:", coreString,
       " MSL:", altitudeString,
-      " nearZ:", nearZ,
-      " farZ:", farZ
+      " Speed:", speedString,
+//      " nearZ:", nearZ,
+//      " farZ:", farZ
     )
   }
   
@@ -261,11 +281,16 @@ final class Renderer: NSObject, MTKViewDelegate {
         threadsPerMeshThreadgroup: mThreadsPerMesh
       )
       renderEncoder.endEncoding()
-      if let drawable = view.currentDrawable {
-        commandBuffer.present(drawable)
-      }
+    }
+    if let drawable = view.currentDrawable {
+      commandBuffer.present(drawable)
+    }
+    commandBuffer.addCompletedHandler { buffer in
+      self.lastGPUEndTime = buffer.gpuEndTime
     }
     commandBuffer.commit()
+    commandBuffer.waitUntilCompleted()
+    physics.step(time: self.lastGPUEndTime)
   }
   
   func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
