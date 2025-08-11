a199e79aa51097eb4b0f39bae80926cbeb9f8c82
Dan Cutting <dan@cutting.io>
2025-08-11 17:42:45 +1000

    Tidy.

--- CubeSphere macOS/AppDelegate.swift
+++ CubeSphere macOS/AppDelegate.swift
@@ -1,30 +1,7 @@
-//
-//  AppDelegate.swift
-//  CubeSphere macOS
-//
-//  Created by Dan Cutting on 7/1/2024.
-//  Copyright © 2024 cutting.io. All rights reserved.
-//
-
 import Cocoa
 
-@main
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
-    }
-
-
+@main class AppDelegate: NSObject, NSApplicationDelegate {
+  func applicationDidFinishLaunching(_ aNotification: Notification) {}
+  func applicationWillTerminate(_ aNotification: Notification) {}
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { true }
 }
-
